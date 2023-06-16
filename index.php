683a0c0c1ce04b9270f3bdf14f21222f2ba86633
Isaacse <125591950+Isaacse@users.noreply.github.com>
2023-06-16 09:25:37 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php
+    $color1 = "amarillo";
+    $color2 = "azul";
+    $color3 = "rojo";
+
+    echo "<h2>el platano es $color1 <br>";
+
+    echo "<h2>el carro es de color $color2 <br>";
+
+    echo "<h2>las flores son de color $color3 <br>";
+    ?> 
+  </body>
+</html>
